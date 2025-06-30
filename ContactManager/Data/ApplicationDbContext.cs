65fa9364cf45593d0065202c4105ff7c14fae779
ana.gomes <ana.gomes@tceal.tc.br>
2025-06-30 20:07:44 -0300

    context and seed data

---
+++ ContactManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
+
+namespace ContactManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) { }
+
+        public DbSet<Customers> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Customers>()
+            //    .HasIndex(c => c.EmailAddress)
+            //    .IsUnique();
+
+            //modelBuilder.Entity<Customers>()
+            //    .HasIndex(c => c.Contact)
+            //    .IsUnique();
+        }
+    }
+}
---
+++ ContactManager/Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data
+{
+    public class DbInitializer 
+    {
+        public static void Initialize(ApplicationDbContext context) {
+        
+            if(context.Customers == null) return;
+
+            var customers = new Customers[]
+            {
+                 new Customers
+                {
+                    Name = "João Silva",
+                    //Contact = "999999999",
+                    //Email = "joao@example.com"
+                },
+                new Customers
+                {
+                    Name = "Maria Oliveira",
+                    //Contact = "888888888",
+                    //Email = "maria@example.com"
+                },
+                new Customers
+                {
+                    Name = "Ana Costa",
+                    //Contact = "777777777",
+                    //Email = "ana@example.com"
+                }
+            };
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+    }
+}

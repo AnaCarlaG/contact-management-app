b5ba7b920dc3faad1b432da9532d10686fecb630
ana.gomes <ana.gomes@tceal.tc.br>
2025-06-30 17:29:06 -0300

    model inicial do projeto

---
+++ ContactManager/Models/Customers.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "O nome deve ter pelo menos 6 caracteres.")]
+        public string Name { get; set; }
+        //[Required]
+        //[RegularExpression(@"^\d{9}", ErrorMessage ="O número de contato deve conter exatamente 9 digítos.")]
+        //public string Contact { get; set; }
+        //[Required]
+        //[EmailAddress(ErrorMessage ="E-mail inválido.")]
+        //public string EmailAddress { get; set; }
+        //public bool isDeleted { get; set; } = false;
+    }
+}

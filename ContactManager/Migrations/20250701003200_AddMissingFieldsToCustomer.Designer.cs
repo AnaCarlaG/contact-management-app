d2c31c07f5252d2255b3119bcde19858bbddfa8f
ana.gomes <ana.gomes@tceal.tc.br>
2025-06-30 22:51:16 -0300

    add os campos que estavam faltando

--- ContactManager/Migrations/20250701003200_AddMissingFieldsToCustomer.Designer.cs
+++ ContactManager/Migrations/20250701003200_AddMissingFieldsToCustomer.Designer.cs
@@ -2,6 +2,7 @@
 using ContactManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContactManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250701003200_AddMissingFieldsToCustomer")]
+    partial class AddMissingFieldsToCustomer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ContactManager/Migrations/20250701003200_AddMissingFieldsToCustomer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManager.Migrations
+{
+    public partial class AddMissingFieldsToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "Customers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Customers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Customers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Customers");
+        }
+    }
+}

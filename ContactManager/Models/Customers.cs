54d78423468410df30379ff2a20e8d6469796142
ana.gomes <ana.gomes@tceal.tc.br>
2025-06-30 22:52:07 -0300

    ajuste na model para as validacoes

--- ContactManager/Models/Customers.cs
+++ ContactManager/Models/Customers.cs
@@ -6,15 +6,16 @@ namespace ContactManager.Models
     public class Customers
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Name is mandatory")]
         [MinLength(6, ErrorMessage = "The name needs to have 6 characters")]
         public string Name { get; set; }
-        //[Required]
-        //[RegularExpression(@"^\d{9}", ErrorMessage ="The contact number shoud be 9 digits")]
-        //public string Contact { get; set; }
-        //[Required]
-        //[EmailAddress(ErrorMessage ="Invalid e-mail")]
-        //public string EmailAddress { get; set; }
-        //public bool isDeleted { get; set; } = false;
+        [Required(ErrorMessage = "Contact number is mandatory")]
+        [RegularExpression(@"^\d{9}", ErrorMessage = "The contact number shoud be 9 digits")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "E-mail is mandatory")]
+        [EmailAddress(ErrorMessage = "Invalid e-mail")]
+        public string EmailAddress { get; set; }
+        public bool isDeleted { get; set; } = false;
     }
 }
